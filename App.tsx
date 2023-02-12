62e3dda8fb3ceda299960dac854993771ab63c8f
Matheus <matheus.augustoreal@gmail.com>
2023-02-12 20:25:36 -0300

    feat: added Roboto font

--- App.tsx
+++ App.tsx
@@ -1,8 +1,15 @@
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
 import { NativeBaseProvider } from "native-base";
 import { SignIn } from "./src/screens/SignIn";
 import { THEME } from "./src/styles/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <NativeBaseProvider theme={THEME}>
       <SignIn />
